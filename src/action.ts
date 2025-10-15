442e0b8a2654550dd10f7583a0f969c477248789
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-10-15 10:11:30 +0200

    action.ts aktualisieren

--- src/action.ts
+++ src/action.ts
@@ -7,3 +7,6 @@ console.log('version:', versionInput);
 actionsCore.setOutput('html', '<h1>HTML<h1>');
 actionsCore.setOutput('text', '<h1>Text<h1>');
 actionsCore.setOutput('json', JSON.stringify({author: "Author"}));
+
+// https://feedback.minecraft.net/hc/en-us/sections/360002267532-Snapshot-Information-and-Changelogs
+// https://feedback.minecraft.net/hc/en-us/sections/360001186971-Release-Changelogs
