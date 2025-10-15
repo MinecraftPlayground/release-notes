dbdb3d08ef12013d4c379e1aae0712c734dfed8f
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-10-15 10:28:14 +0200

    Add deno-dom for HTML parsing and fetch snapshot changelogs
    Added @b-fuze/deno-dom to dependencies and imported it in action.ts. Implemented fetching and parsing of the Minecraft snapshot changelogs page, logging the document's text content for further processing.

--- src/action.ts
+++ src/action.ts
@@ -1,4 +1,5 @@
 import actionsCore from '@actions/core';
+import * as dom from '@b-fuze/deno-dom'
 
 const versionInput = actionsCore.getInput('version');
 
@@ -7,6 +8,14 @@ console.log('version:', versionInput);
 actionsCore.setOutput('html', '<h1>HTML<h1>');
 actionsCore.setOutput('text', '<h1>Text<h1>');
 actionsCore.setOutput('json', JSON.stringify({author: "Author"}));
+
+const document = new dom.DOMParser().parseFromString(
+  await fetch('https://feedback.minecraft.net/hc/en-us/sections/360002267532-Snapshot-Information-and-Changelogs')
+    .then(response => response.text()),
+  'text/html'
+)
+
+console.log(document.textContent)
 
 // https://feedback.minecraft.net/hc/en-us/sections/360002267532-Snapshot-Information-and-Changelogs
 // https://feedback.minecraft.net/hc/en-us/sections/360001186971-Release-Changelogs
